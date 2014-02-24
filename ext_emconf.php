f931ee996f77166060f2c04648852e77fad6c9d6
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-02-24 12:17:58 +0100

    [TASK] Update depedencies to EXT:media
    Change-Id: I6b076bc627193c967d2b48b939cc21e3d0b40589

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = array (
     'depends' => 
     array (
       'cms' => '',
-      'media' => '1.0.0-1.1.99',
+      'media' => '1.0.0-2.0.99',
       'typo3' => '6.1.6-6.1.99',
     ),
     'conflicts' => 
