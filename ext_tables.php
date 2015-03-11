62fb3ee31f5bdd6a85cd08cded94925c60b6100b
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-03-11 15:39:47 +0100

    [TASK] Remove deprecated usage of signal / slot IndexerService::postFileIndex
    Resolves #1

--- ext_tables.php
+++ ext_tables.php
@@ -5,8 +5,3 @@ if (!defined('TYPO3_MODE')) {
 
 /** @var \TYPO3\CMS\Extbase\Object\ObjectManager $objectManager */
 $objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
-
-// Connect "postFileIndex" signal slot with the metadata service.
-/** @var $signalSlotDispatcher \TYPO3\CMS\Extbase\SignalSlot\Dispatcher */
-$signalSlotDispatcher = $objectManager->get('TYPO3\CMS\Extbase\SignalSlot\Dispatcher');
-$signalSlotDispatcher->connect('TYPO3\CMS\Core\Resource\Service\IndexerService', 'postFileIndex', 'Fab\Metadata\Service\IndexerService', 'postFileIndex', FALSE);
