1d2126a2dd5c0b07a7c3093b572376a3105725dd
Stephan Großberndt <s.grossberndt@sidebysite.de>
2018-02-22 15:03:56 +0100

    Change focal_length from int to float

--- ext_tables.sql
+++ ext_tables.sql
@@ -7,7 +7,7 @@ CREATE TABLE sys_file_metadata (
 	shutter_speed_value varchar(24) DEFAULT '' NOT NULL,
 	iso_speed_ratings varchar(24) DEFAULT '' NOT NULL,
 	camera_model varchar(255) DEFAULT '' NOT NULL,
-	focal_length int(4) unsigned DEFAULT '0' NOT NULL,
+	focal_length float unsigned DEFAULT '0' NOT NULL,
 	flash int(4) DEFAULT '0' NOT NULL,
 	metering_mode int(4) DEFAULT '0' NOT NULL,
 	horizontal_resolution int(8) DEFAULT '0' NOT NULL,
