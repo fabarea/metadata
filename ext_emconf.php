f0a59e6e850fe66ad4b8891da350ad047a00c04b
Fabien Udriot <fabien@omic.ch>
2018-04-04 09:50:54 +0200

    [CLEANUP] PSR-2 reformat PHP code

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,27 +1,26 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-  'title' => 'Metadata extraction',
-  'description' => 'PHP-based metadata extraction. The extension relies on the new Metadata Extractor API introduced in TYPO3 CMS 6.2.',
-  'category' => 'service',
-  'author' => 'Fabien Udriot',
-  'author_email' => 'fabien.udriot@typo3.org',
-  'author_company' => 'Ecodev',
-  'state' => 'stable',
-  'version' => '2.3.0-dev',
-  'constraints' =>
-  [
-    'depends' =>
-    [
-      'cms' => '',
-      'typo3' => '6.2.0-8.7.99',
-      'filemetadata' => '',
-    ],
-    'conflicts' =>
-    [
-    ],
-    'suggests' =>
-    [
-    ],
-  ],
+    'title' => 'Metadata extraction',
+    'description' => 'PHP-based metadata extraction. The extension relies on the new Metadata Extractor API introduced in TYPO3 CMS 6.2.',
+    'category' => 'service',
+    'author' => 'Fabien Udriot',
+    'author_email' => 'fabien.udriot@typo3.org',
+    'author_company' => 'Ecodev',
+    'state' => 'stable',
+    'version' => '2.3.0-dev',
+    'constraints' =>
+        [
+            'depends' =>
+                [
+                    'typo3' => '6.2.0-8.7.99',
+                    'filemetadata' => '0.0.0-0.0.0',
+                ],
+            'conflicts' =>
+                [
+                ],
+            'suggests' =>
+                [
+                ],
+        ],
 ];
