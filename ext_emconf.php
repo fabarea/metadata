3c11e342325f9db0c6fb98273f5489de3c040220
jkummer <service@enobe.de>
2019-03-06 19:15:02 +0100

    [TASK] Compatibility for TYPO3 CMS 9.5 refs #29
    - solve deprecated extension configuration check
    - adjust extension configuration user-function class loading
    - improve rendering for extension configuration
    
    Backward compatibility down to TYPO3 CMS 6.2 is preserved and tested

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
         [
             'depends' =>
                 [
-                    'typo3' => '6.2.0-8.7.99',
+                    'typo3' => '6.2.0-9.5.99',
                     'filemetadata' => '0.0.0-0.0.0',
                 ],
             'conflicts' =>
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,21 +1,36 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die ('Access denied.');
 }
 
-$extractMetadataImage = $extractMetadataPdf = TRUE;
-if (!empty($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['metadata'])) {
-	$configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['metadata']);
-	$extractMetadataImage = (bool)$configuration['extract_image_metadata'];
-	$extractMetadataPdf = (bool)$configuration['extract_pdf_metadata'];
+$extractMetadataImage = $extractMetadataPdf = true;
+
+// Get extension configuration
+if (class_exists(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)) {
+    $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
+    );
+    $configuration = $extensionConfiguration->get('metadata');
+} else {
+    // @extensionScannerIgnoreLine Fallback to access extConf for TYPO3 CMS version below 9.5
+    if (isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['metadata'])
+        && !empty($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['metadata'])
+    ) {
+        $configuration = $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['metadata'];
+        if (!is_array($configuration)) {
+            $configuration = unserialize($configuration);
+        }
+    }
 }
 
 // Register metadata extractor for images if configured so.
-if ( $extractMetadataImage) {
-	\TYPO3\CMS\Core\Resource\Index\ExtractorRegistry::getInstance()->registerExtractionService('Fab\Metadata\Index\ImageMetadataExtractor');
+if (isset($configuration['extract_image_metadata']) && (bool)$configuration['extract_image_metadata']) {
+    \TYPO3\CMS\Core\Resource\Index\ExtractorRegistry::getInstance()
+        ->registerExtractionService('Fab\Metadata\Index\ImageMetadataExtractor');
 }
 
 // Register metadata extractor for pdf if configured so.
-if ( $extractMetadataPdf) {
-	\TYPO3\CMS\Core\Resource\Index\ExtractorRegistry::getInstance()->registerExtractionService('Fab\Metadata\Index\PdfMetadataExtractor');
+if (isset($configuration['extract_pdf_metadata']) && (bool)$configuration['extract_pdf_metadata']) {
+    \TYPO3\CMS\Core\Resource\Index\ExtractorRegistry::getInstance()
+        ->registerExtractionService('Fab\Metadata\Index\PdfMetadataExtractor');
 }
