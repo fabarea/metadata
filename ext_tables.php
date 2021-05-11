85fa6ab907a1f4f2ddca9437d5ebff4d841c6e40
Stephan Großberndt <s.grossberndt@sidebysite.de>
2021-05-11 20:22:33 +0200

    [TASK] Remove unused ext_tables.php

--- ext_tables.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
-}
-
-/** @var \TYPO3\CMS\Extbase\Object\ObjectManager $objectManager */
-$objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
