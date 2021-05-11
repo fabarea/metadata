d6c2791b062bbe58edca1a0b142aec50a1630778
Stephan Großberndt <s.grossberndt@sidebysite.de>
2021-05-11 18:07:29 +0200

    [BUGFIX] Handle empty GPS coordinates correctly
    Defaults any empty value to '0.00000000000000' in order to avoid issues inserting the database record

--- Classes/Index/ImageMetadataExtractor.php
+++ Classes/Index/ImageMetadataExtractor.php
@@ -396,7 +396,7 @@ class ImageMetadataExtractor extends AbstractExtractor {
 	 * @param array $value
 	 * @param string $ref
 	 *
-	 * @return string|null
+	 * @return string
 	 */
 	protected function parseGpsCoordinate($value, $ref) {
 		if (is_array($value)) {
@@ -415,7 +415,7 @@ class ImageMetadataExtractor extends AbstractExtractor {
 			$value = ($ref === 'N' || $ref === 'E') ? $neutralValue : '-' . $neutralValue;
 		}
 
-		return $value === null ? null : (string)$value;
+		return empty($value) ? '0.00000000000000' : (string)$value;
 	}
 
 	/**
