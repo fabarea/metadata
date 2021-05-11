98e923f82bbc28853a398b7dba7fa1c5562e0b22
Stephan Großberndt <s.grossberndt@sidebysite.de>
2021-05-11 20:26:18 +0200

    [TASK] Clean up Configuration/TCA/Overrides/sys_file_metadata.php
    - PHP7 syntax
    - Remove version switch for unsupported versions < TYPO3 v8
    - Consistent formatting of constants true/false/null as lowercase

--- Configuration/TCA/Overrides/sys_file_metadata.php
+++ Configuration/TCA/Overrides/sys_file_metadata.php
@@ -1,235 +1,208 @@
 <?php
-if (!defined('TYPO3_MODE')) die ('Access denied.');
+defined('TYPO3_MODE') || die('Access denied.');
 
-$tca = array(
-	'palettes' => array(
-		'20' => array('showitem' => 'publisher, source', 'canNotCollapse' => '1'),
-		'21' => array('showitem' => 'creator_tool, copyright_notice', 'canNotCollapse' => '1'),
-		'22' => array(
+$tca = [
+	'palettes' => [
+		'20' => ['showitem' => 'publisher, source', 'canNotCollapse' => '1'],
+		'21' => ['showitem' => 'creator_tool, copyright_notice', 'canNotCollapse' => '1'],
+		'22' => [
 			'showitem' => 'iso_speed_ratings, aperture_value, shutter_speed_value, focal_length',
 			'canNotCollapse' => '1'
-		),
-		'23' => array('showitem' => 'camera_model, flash, metering_mode', 'canNotCollapse' => '1'),
-		'51' => array('showitem' => 'horizontal_resolution, vertical_resolution', 'canNotCollapse' => '1'),
-		'61' => array('showitem' => 'credit', 'canNotCollapse' => '1'),
-	),
-	'columns' => array(
-		'credit' => array(
+		],
+		'23' => ['showitem' => 'camera_model, flash, metering_mode', 'canNotCollapse' => '1'],
+		'51' => ['showitem' => 'horizontal_resolution, vertical_resolution', 'canNotCollapse' => '1'],
+		'61' => ['showitem' => 'credit', 'canNotCollapse' => '1'],
+	],
+	'columns' => [
+		'credit' => [
 			'exclude' => 1,
 			'l10n_display' => 'defaultAsReadonly',
 			'label' => 'LLL:EXT:metadata/Resources/Private/Language/locallang.xlf:sys_file_metadata.credit',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => 30,
 				'max' => '32',
 				'eval' => 'trim'
-			),
-		),
-		'copyright_notice' => array(
+			],
+		],
+		'copyright_notice' => [
 			'exclude' => 1,
 			'l10n_display' => 'defaultAsReadonly',
 			'label' => 'LLL:EXT:metadata/Resources/Private/Language/locallang.xlf:sys_file_metadata.copyright_notice',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => 40,
 				'max' => '255',
 				'eval' => 'trim'
-			),
-		),
-		'aperture_value' => array(
+			],
+		],
+		'aperture_value' => [
 			'exclude' => 1,
 			'l10n_mode' => 'exclude',
 			'l10n_display' => 'defaultAsReadonly',
 			'label' => 'LLL:EXT:metadata/Resources/Private/Language/locallang.xlf:sys_file_metadata.aperture_value',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => '7',
 				'eval' => 'float',
 				'default' => '0',
-				'readOnly' => TRUE,
-			),
-		),
-		'shutter_speed_value' => array(
+				'readOnly' => true,
+			],
+		],
+		'shutter_speed_value' => [
 			'exclude' => 1,
 			'l10n_mode' => 'exclude',
 			'l10n_display' => 'defaultAsReadonly',
 			'label' => 'LLL:EXT:metadata/Resources/Private/Language/locallang.xlf:sys_file_metadata.shutter_speed_value',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => '7',
 				'max' => '24',
 				'eval' => 'trim',
 				'default' => '0',
-				'readOnly' => TRUE,
-			),
-		),
-		'iso_speed_ratings' => array(
+				'readOnly' => true,
+			],
+		],
+		'iso_speed_ratings' => [
 			'exclude' => 1,
 			'l10n_mode' => 'exclude',
 			'l10n_display' => 'defaultAsReadonly',
 			'label' => 'LLL:EXT:metadata/Resources/Private/Language/locallang.xlf:sys_file_metadata.iso_speed_ratings',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => '7',
 				'max' => '24',
 				'eval' => 'trim',
 				'default' => '0',
-				'readOnly' => TRUE,
-			),
-		),
-		'camera_model' => array(
+				'readOnly' => true,
+			],
+		],
+		'camera_model' => [
 			'exclude' => 1,
 			'l10n_mode' => 'exclude',
 			'l10n_display' => 'defaultAsReadonly',
 			'label' => 'LLL:EXT:metadata/Resources/Private/Language/locallang.xlf:sys_file_metadata.camera_model',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => '12',
 				'max' => '255',
 				'eval' => 'trim',
 				'default' => '',
-				'readOnly' => TRUE,
-			),
-		),
-		'focal_length' => array(
+				'readOnly' => true,
+			],
+		],
+		'focal_length' => [
 			'exclude' => 1,
 			'l10n_mode' => 'exclude',
 			'l10n_display' => 'defaultAsReadonly',
 			'label' => 'LLL:EXT:metadata/Resources/Private/Language/locallang.xlf:sys_file_metadata.focal_length',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => '7',
 				'max' => '24',
 				'eval' => 'int',
 				'default' => '0',
-				'readOnly' => TRUE,
-			),
-		),
-		'flash' => array(
+				'readOnly' => true,
+			],
+		],
+		'flash' => [
 			'exclude' => 1,
 			'l10n_mode' => 'exclude',
 			'l10n_display' => 'defaultAsReadonly',
 			'label' => 'LLL:EXT:metadata/Resources/Private/Language/locallang.xlf:sys_file_metadata.flash',
-			'config' => array(
+			'config' => [
 				'type' => 'select',
 				'renderType' => 'selectSingle',
 				'default' => '-1',
-				'items' => array(
-					array('', '0'),
-					array('Flash', '1'),
-					array('Flash, strobe return light not detected', '5'),
-					array('Flash, strobe return light detected', '7'),
-					array('Compulsory Flash', '9'),
-					array('Compulsory Flash, Return light not detected', '13'),
-					array('Compulsory Flash, Return light detected', '15'),
-					array('No Flash', '16'),
-					array('No Flash', '24'),
-					array('Flash, Auto-Mode', '25'),
-					array('Flash, Auto-Mode, Return light not detected', '29'),
-					array('Flash, Auto-Mode, Return light detected', '31'),
-					array('No Flash', '32'),
-					array('Red Eye','65'),
-					array('Red Eye, Return light not detected','69'),
-					array('Red Eye, Return light detected', '71'),
-					array('Red Eye, Compulsory Flash', '73'),
-					array('Red Eye, Compulsory Flash, Return light not detected', '77'),
-					array('Red Eye, Compulsory Flash, Return light detected', '79'),
-					array('Red Eye, Auto-Mode', '89'),
-					array('Red Eye, Auto-Mode, Return light not detected', '93'),
-					array('Red Eye, Auto-Mode, Return light detected', '95'),
-				),
-				'readOnly' => TRUE,
-			),
-		),
-		'metering_mode' => array(
+				'items' => [
+					['', '0'],
+					['Flash', '1'],
+					['Flash, strobe return light not detected', '5'],
+					['Flash, strobe return light detected', '7'],
+					['Compulsory Flash', '9'],
+					['Compulsory Flash, Return light not detected', '13'],
+					['Compulsory Flash, Return light detected', '15'],
+					['No Flash', '16'],
+					['No Flash', '24'],
+					['Flash, Auto-Mode', '25'],
+					['Flash, Auto-Mode, Return light not detected', '29'],
+					['Flash, Auto-Mode, Return light detected', '31'],
+					['No Flash', '32'],
+					['Red Eye', '65'],
+					['Red Eye, Return light not detected', '69'],
+					['Red Eye, Return light detected', '71'],
+					['Red Eye, Compulsory Flash', '73'],
+					['Red Eye, Compulsory Flash, Return light not detected', '77'],
+					['Red Eye, Compulsory Flash, Return light detected', '79'],
+					['Red Eye, Auto-Mode', '89'],
+					['Red Eye, Auto-Mode, Return light not detected', '93'],
+					['Red Eye, Auto-Mode, Return light detected', '95'],
+				],
+				'readOnly' => true,
+			],
+		],
+		'metering_mode' => [
 			'exclude' => 1,
 			'l10n_mode' => 'exclude',
 			'l10n_display' => 'defaultAsReadonly',
 			'label' => 'LLL:EXT:metadata/Resources/Private/Language/locallang.xlf:sys_file_metadata.metering_mode',
-			'config' => array(
+			'config' => [
 				'type' => 'select',
 				'default' => '-1',
 				'renderType' => 'selectSingle',
 				'itemListStyle' => 'width:200px;',
-				'items' => array(
-					array('', '0'),
-					array('Average', '1'),
-					array('Center Weighted Average', '2'),
-					array('Spot', '3'),
-					array('Multi-Spot', '4'),
-					array('Pattern', '5'),
-					array('Partial', '6'),
-					array('Other', '255'),
-				),
-				'readOnly' => TRUE,
-			),
-		),
-		'horizontal_resolution' => array(
+				'items' => [
+					['', '0'],
+					['Average', '1'],
+					['Center Weighted Average', '2'],
+					['Spot', '3'],
+					['Multi-Spot', '4'],
+					['Pattern', '5'],
+					['Partial', '6'],
+					['Other', '255'],
+				],
+				'readOnly' => true,
+			],
+		],
+		'horizontal_resolution' => [
 			'exclude' => 1,
 			'l10n_mode' => 'exclude',
 			'l10n_display' => 'defaultAsReadonly',
 			'label' => 'LLL:EXT:metadata/Resources/Private/Language/locallang.xlf:sys_file_metadata.horizontal_resolution',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => '10',
 				'max' => '8',
 				'eval' => 'int',
 				'default' => '0',
-				'readOnly' => TRUE,
-			),
-		),
-		'vertical_resolution' => array(
+				'readOnly' => true,
+			],
+		],
+		'vertical_resolution' => [
 			'exclude' => 1,
 			'l10n_mode' => 'exclude',
 			'l10n_display' => 'defaultAsReadonly',
 			'label' => 'LLL:EXT:metadata/Resources/Private/Language/locallang.xlf:sys_file_metadata.vertical_resolution',
-			'config' => array(
+			'config' => [
 				'type' => 'input',
 				'size' => '10',
 				'max' => '8',
 				'eval' => 'int',
 				'default' => '0',
-				'readOnly' => TRUE,
-			),
-		),
+				'readOnly' => true,
+			],
+		],
 		// existing fields
-		'color_space' => array(
-			'config' => array(
-				'items' => array(
-					array('sRGB', 'sRGB'),
-				),
-			)
-		),
-	),
-);
-
-// "types" are not the same between 7.x and 6.2.
-if (version_compare(TYPO3_version, '7.1', '<')) {
-	$tca['types'] = array(
-		TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => array('showitem' => '
-			fileinfo, title, description, alternative, keywords, caption, download_name,' .
-			'--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.access,
-				--palette--;LLL:EXT:filemetadata/Resources/Private/Language/locallang_tca.xlf:palette.visibility;10;;,
-				fe_groups,
-			--div--;LLL:EXT:filemetadata/Resources/Private/Language/locallang_tca.xlf:tabs.metadata,
-				creator,
-				--palette--;;20;;,
-				--palette--;;21;;,
-				--palette--;;61;;,
-				--palette--;LLL:EXT:metadata/Resources/Private/Language/locallang.xlf:palette.exif;22;;,
-				--palette--;;23;;,
-				--palette--;LLL:EXT:filemetadata/Resources/Private/Language/locallang_tca.xlf:palette.geo_location;40;;,
-				--palette--;;30;;,
-				--palette--;LLL:EXT:filemetadata/Resources/Private/Language/locallang_tca.xlf:palette.metrics;50;;,
-				--palette--;;51;;,
-				--palette--;LLL:EXT:filemetadata/Resources/Private/Language/locallang_tca.xlf:palette.content_date;60,
-			--div--;LLL:EXT:lang/locallang_tca.xlf:sys_category.tabs.category,categories'
-		),
-	);
-} else {
-	// TCA for > 7.1
-	$tca['types'] = array(
-		TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => array('showitem' => '
+		'color_space' => [
+			'config' => [
+				'items' => [
+					['sRGB', 'sRGB'],
+				],
+			]
+		],
+	],
+	'types' => [
+		TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => ['showitem' => '
 			fileinfo, title, description, alternative, keywords, caption, download_name,
 			--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,
 				--palette--;LLL:EXT:filemetadata/Resources/Private/Language/locallang_tca.xlf:palette.visibility;10;;,
@@ -247,8 +220,8 @@ if (version_compare(TYPO3_version, '7.1', '<')) {
 				--palette--;;51;;,
 				--palette--;LLL:EXT:filemetadata/Resources/Private/Language/locallang_tca.xlf:palette.content_date;60,
 			--div--;LLL:EXT:lang/locallang_tca.xlf:sys_category.tabs.category,categories'
-		),
-	);
-}
+		],
+	]
+];
 
 \TYPO3\CMS\Core\Utility\ArrayUtility::mergeRecursiveWithOverrule($GLOBALS['TCA']['sys_file_metadata'], $tca);
