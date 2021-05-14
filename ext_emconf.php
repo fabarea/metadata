71468caa2878cfc92d4f1f589899548865eaaf05
Fabien Udriot <fabien@omic.ch>
2021-05-14 21:12:53 +0200

    [TASK] Prepare next release

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'fabien.udriot@typo3.org',
     'author_company' => 'Ecodev',
     'state' => 'stable',
-    'version' => '4.0.0',
+    'version' => '4.1.0-dev',
     'constraints' =>
         [
             'depends' =>
